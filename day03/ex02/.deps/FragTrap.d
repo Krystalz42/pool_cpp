03c7e76d5e305a80a3d9875c89ba107a13763026
Krystalz42 <aroulin@student.42.fr>
2018-10-04 17:35:10 +0200

    Fuck

--- day03/ex02/.deps/FragTrap.d
+++
@@ -1,5 +0,0 @@
-.objs/FragTrap.o: srcs/FragTrap.cpp incs/FragTrap.hpp incs/ClapTrap.hpp
-
-incs/FragTrap.hpp:
-
-incs/ClapTrap.hpp:
--- day03/ex02/.deps/main.d
+++
@@ -1,8 +0,0 @@
-.objs/main.o: srcs/main.cpp incs/ClapTrap.hpp incs/ScavTrap.hpp \
-  incs/FragTrap.hpp
-
-incs/ClapTrap.hpp:
-
-incs/ScavTrap.hpp:
-
-incs/FragTrap.hpp:
